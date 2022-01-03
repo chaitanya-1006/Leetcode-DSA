e1cc7e289c333dc1ca4ce2c73dfa66b565cba4a1
chaitanya-1006 <96867040+chaitanya-1006@users.noreply.github.com>
2022-01-03 18:52:39 +0530

    Update next-permutation.cpp
    //Improvized code is also added

--- next-permutation/next-permutation.cpp
+++ next-permutation/next-permutation.cpp
@@ -16,6 +16,8 @@ public:
         for(;i>=1;i--)
             if(nums[i]>nums[i-1])
                 break;
+        
+        //array is in Descending order
         if(i==0)
         {
             reverse(nums,0,n);
@@ -28,8 +30,17 @@ public:
             if(nums[j]>nums[i-1])
                 smallest=j;
         }
-            
         swap(nums[smallest],nums[i-1]);
+        
+        //ex: 534976
+        
+// If we traverse from right to left for j, the 1st element we found will be the smallest one comparing to those on the left of the element
+//         int j=n;
+//         for(;j>=i;j--)
+//             if(nums[j]>nums[i-1])
+//                 break;
+//         swap(nums[j],nums[i-1]);
+        
         reverse(nums,i,n);
     }
 };

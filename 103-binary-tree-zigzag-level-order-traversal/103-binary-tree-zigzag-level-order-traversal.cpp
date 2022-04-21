acc30cfb55c387e471b89716f47bb314f36ddad2
chaitanya-1006 <96867040+chaitanya-1006@users.noreply.github.com>
2022-04-21 11:13:14 +0530

    Time: 4 ms (58.42%), Space: 12.1 MB (76.50%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,38 +1,58 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
-        vector<vector<int>>ans;
-        if(root==NULL){
+        vector<vector<int>> ans;
+        if(!root)
             return ans;
-        }
-        queue<TreeNode*>q;
+        TreeNode* temp;
+        
+        queue<TreeNode*> q;
         q.push(root);
-        bool flag = false;
-        while(!q.empty()){
-            int size = q.size();
-            vector<int>level;
-           
-           while(size--){
-                TreeNode*node = q.front();
+        
+        int level=0;
+        
+        
+        while(!q.empty())
+        {
+            
+            vector<int> levelWise;
+            int n=q.size();
+            
+            for(int i=0;i<n;i++)
+            {
+                TreeNode* temp=q.front();
+                levelWise.push_back(temp->val);
+                if(temp->left)
+                    q.push(temp->left);
+                if(temp->right)
+                    q.push(temp->right);
+                
                 q.pop();
-                level.push_back( node->val);
-                if(node->left != NULL){
-                q.push(node->left);
-            }
-                if(node->right){
-                q.push(node->right);
-                }
                 
             }
-            
-            
-            flag = !flag;
-            if(flag==false){
-                reverse(level.begin(),level.end());
+            if(level%2==1)
+            {
+                reverse(levelWise.begin(),levelWise.end());
             }
-            ans.push_back(level);
+            
+            level++;
+            ans.push_back(levelWise);
+            
+            
             
         }
+        
         return ans;
         
     }

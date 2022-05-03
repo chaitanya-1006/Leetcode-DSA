7b05a1bce0299da7fbbf7bd0441971bc33a85e94
chaitanya-1006 <96867040+chaitanya-1006@users.noreply.github.com>
2022-05-03 15:32:32 +0530

    Time: 119 ms (35.72%), Space: 28.4 MB (93.06%) - LeetHub

--- 912-sort-an-array/912-sort-an-array.cpp
+++ 912-sort-an-array/912-sort-an-array.cpp
@@ -1,21 +1,22 @@
 class Solution {
 public:
     int size;
-    int parent(int i)
-    {
-        return (i-1)/2;
-    }
-    int left(int i)
-    {
-        return (2*i)+1;
-    }
-    int right(int i)
-    {
-        return (2*i)+2;
-    }
+    // int parent(int i)
+    // {
+    //     return (i-1)/2;
+    // }
+    // int left(int i)
+    // {
+    //     return (2*i)+1;
+    // }
+    // int right(int i)
+    // {
+    //     return (2*i)+2;
+    // }
     void maxHeapify(int i,vector<int>& nums,int n)
     {
-        int lt=left(i),rt=right(i);
+        //int lt=left(i),rt=right(i);
+        int lt=(2*i)+1,rt=(2*i)+2;
         int largest=i;
         if(lt<n && nums[lt]>nums[i])
             largest=lt;
@@ -47,7 +48,8 @@ public:
         buildHeap(nums);
         vector<int> ans;
       
-        // we are making 
+        // repeatedly swaping  root with the lastnode, reduce the heap size by 1 and heapify
+        
         
         for(int i=size-1;i>0;i--)
         {
